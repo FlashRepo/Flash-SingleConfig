fb4183d609a6937f92b48a934dd4c25a4c4853a0
Vivek <vivekaxl@gmail.com>
2017-03-30 04:49:55 -0400

    First push

---
+++ progressive_active_learning.py
@@ -0,0 +1,135 @@
+from __future__ import division
+import pandas as pd
+import sys
+from os import listdir
+from random import shuffle
+from sklearn.tree import DecisionTreeRegressor
+from policies import policy1, policy2
+import numpy as np
+
+
+class solution_holder:
+    def __init__(self, id, decisions, objective, rank):
+        self.id = id
+        self.decision = decisions
+        self.objective = objective
+        self.rank = rank
+
+
+def get_data(filename, initial_size):
+    """
+    :param filename:
+    :param Initial training size
+    :return: Training and Testing
+    """
+    pdcontent = pd.read_csv(filename)
+    indepcolumns = [col for col in pdcontent.columns if "$<" not in col]
+    depcolumns = [col for col in pdcontent.columns if "$<" in col]
+    sortpdcontent = pdcontent.sort_values(by=depcolumns[-1])
+    ranks = {}
+    for i, item in enumerate(sorted(set(sortpdcontent[depcolumns[-1]].tolist()))):
+        ranks[item] = i
+
+    content = list()
+    for c in xrange(len(sortpdcontent)):
+        content.append(solution_holder(
+                                       c,
+                                       sortpdcontent.iloc[c][indepcolumns].tolist(),
+                                       sortpdcontent.iloc[c][depcolumns].tolist(),
+                                       ranks[sortpdcontent.iloc[c][depcolumns].tolist()[-1]]
+                                       )
+                       )
+
+    shuffle(content)
+    indexes = range(len(content))
+    train_indexes, test_indexes = indexes[:initial_size],  indexes[initial_size:]
+    assert(len(train_indexes) + len(test_indexes) == len(indexes)), "Something is wrong"
+    train_set = [content[i] for i in train_indexes]
+    test_set = [content[i] for i in test_indexes]
+
+    return [train_set, test_set]
+
+
+def get_best_configuration_id(train, test):
+    train_independent = [t.decision for t in train]
+    train_dependent = [t.objective[-1] for t in train]
+
+    test_independent = [t.decision for t in test]
+
+    model = DecisionTreeRegressor()
+    model.fit(train_independent, train_dependent)
+    predicted = model.predict(test_independent)
+    predicted_id = [[t.id,p] for t,p in zip(test, predicted)]
+    predicted_sorted = sorted(predicted_id, key=lambda x: x[-1])
+    # Find index of the best predicted configuration
+    best_index = predicted_sorted[0][0]
+    return best_index
+
+
+def run_active_learning(filename, initial_size, max_lives=10):
+    steps = 0
+    lives = max_lives
+    training_set, testing_set = get_data(filename, initial_size)
+    dataset_size = len(training_set) + len(testing_set)
+    while (initial_size+steps) < dataset_size - 1:
+        best_id = get_best_configuration_id(training_set, testing_set)
+        # print best_index, len(testing_set)
+        best_solution = [t for t in testing_set if t.id == best_id][-1]
+
+        list_of_all_solutions = [t.objective[-1] for t in training_set]
+        if best_solution.objective[-1] < min(list_of_all_solutions):
+            lives = max_lives
+        else:
+            lives -= 1
+        training_set.append(best_solution)
+        # find index of the best_index
+        best_index = [i for i in xrange(len(testing_set)) if testing_set[i].id == best_id]
+        assert(len(best_index) == 1), "Something is wrong"
+        best_index = best_index[-1]
+        del testing_set[best_index]
+        assert(len(training_set) + len(testing_set) == dataset_size), "Something is wrong"
+        if lives == 0:
+            break
+        steps += 1
+
+    return training_set, testing_set
+
+
+def wrapper_run_active_learning(filename, initial_size):
+    training_set, testing_set= run_active_learning(filename, initial_size)
+    global_min = min([t.objective[-1] for t in training_set + testing_set])
+    best_training_solution = [ tt.rank for tt in training_set if min([t.objective[-1] for t in training_set]) == tt.objective[-1]]
+    best_solution = [tt.rank for tt in training_set + testing_set if tt.objective[-1] == global_min]
+    print (min(best_training_solution) - min(best_solution)), len(training_set), " | ",
+    return (min(best_training_solution) - min(best_solution)), len(training_set)
+
+if __name__ == "__main__":
+    filenames = ["./Data/"+f for f in listdir("./Data")]
+    initial_size = 20
+    evals_dict = {}
+    rank_diffs_dict = {}
+    stats_dict = {}
+    for filename in filenames:
+        evals_dict[filename] = []
+        rank_diffs_dict[filename] = []
+        stats_dict[filename] = {}
+        rank_diffs = []
+        evals = []
+        print filename
+        for _ in xrange(20):
+            temp1, temp2 = wrapper_run_active_learning(filename, initial_size)
+            rank_diffs.append(temp1)
+            evals.append(temp2)
+        print
+        evals_dict[filename] = evals
+        rank_diffs_dict[filename] = rank_diffs
+        stats_dict[filename]["mean_rank_diff"] = np.mean(rank_diffs)
+        stats_dict[filename]["std_rank_diff"] = np.std(rank_diffs)
+        stats_dict[filename]["mean_evals"] = np.mean(evals)
+        stats_dict[filename]["std_evals"] = np.std(evals)
+
+    import pickle
+    pickle.dump(evals_dict, "./PickleLocker/ActiveLearning_Evals.p")
+    pickle.dump(rank_diffs_dict, "./PickleLocker/ActiveLearning_Rank_Diff.p")
+    pickle.dump(stats_dict, "./PickleLocker/ActiveLearning_Stats.p")
+
---
+++ progressive_sampling_rank.py
@@ -0,0 +1,185 @@
+from __future__ import division
+import pandas as pd
+import sys
+from os import listdir
+from random import shuffle
+from sklearn.tree import DecisionTreeRegressor
+from policies import policy1, policy2
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+
+
+class solution_holder:
+    def __init__(self, id, decisions, objective, rank):
+        self.id = id
+        self.decision = decisions
+        self.objective = objective
+        self.rank = rank
+
+
+def split_data(filename):
+    pdcontent = pd.read_csv(filename)
+    indepcolumns = [col for col in pdcontent.columns if "$<" not in col]
+    depcolumns = [col for col in pdcontent.columns if "$<" in col]
+    sortpdcontent = pdcontent.sort_values(by=depcolumns[-1])
+    content = list()
+    ranks = {}
+    for i, item in enumerate(sorted(set(sortpdcontent[depcolumns[-1]].tolist()))):
+        ranks[item] = i
+    for c in xrange(len(sortpdcontent)):
+        content.append(solution_holder(
+                                       c,
+                                       sortpdcontent.iloc[c][indepcolumns].tolist(),
+                                       sortpdcontent.iloc[c][depcolumns].tolist(),
+                                       ranks[sortpdcontent.iloc[c][depcolumns].tolist()[-1]]
+                                       )
+                       )
+
+    shuffle(content)
+    indexes = range(len(content))
+    train_indexes, validation_indexes, test_indexes = indexes[:int(0.4*len(indexes))], indexes[int(.4*len(indexes)):int(.6*len(indexes))],  indexes[int(.6*len(indexes)):]
+    assert(len(train_indexes) + len(validation_indexes) + len(test_indexes) == len(indexes)), "Something is wrong"
+    train_set = [content[i] for i in train_indexes]
+    validation_set = [content[i] for i in validation_indexes]
+    test_set = [content[i] for i in test_indexes]
+
+    return [train_set, validation_set, test_set]
+
+
+def mre_progressive(train, test, threshold=0.1):
+    train_independent = [t.decision for t in train]
+    train_dependent = [t.objective[-1] for t in train]
+
+    test_independent = [t.decision for t in test]
+    test_dependent = [t.objective[-1] for t in test]
+
+    model = DecisionTreeRegressor()
+    model.fit(train_independent, train_dependent)
+    predicted = model.predict(test_independent)
+
+    mre = []
+    for org, pred in zip(test_dependent, predicted):
+        mre.append(abs(org - pred)/ abs(org))
+    return np.mean(mre)
+
+
+def rank_progressive(train, test, threshold=4):
+    train_independent = [t.decision for t in train]
+    train_dependent = [t.objective[-1] for t in train]
+
+    sorted_test = sorted(test, key=lambda x: x.objective[-1])
+    for r,st in enumerate(sorted_test): st.rank = r
+    test_independent = [t.decision for t in sorted_test]
+    test_dependent = [t.objective[-1] for t in sorted_test]
+
+    model = DecisionTreeRegressor()
+    model.fit(train_independent, train_dependent)
+    predicted = model.predict(test_independent)
+    predicted_id = [[i,p] for i,p in enumerate(predicted)]
+    predicted_sorted = sorted(predicted_id, key=lambda x: x[-1])
+    # assigning predicted ranks
+    predicted_rank_sorted = [[p[0], p[-1], i] for i,p in enumerate(predicted_sorted)]
+    rank_diffs = [abs(p[0] - p[-1]) for p in predicted_rank_sorted]
+    return np.mean(rank_diffs)
+
+
+def wrapper_rank_progressive(train_set, validation_set):
+    initial_size = 10
+    training_indexes = range(len(train_set))
+    shuffle(training_indexes)
+    sub_train_set = [train_set[i] for i in training_indexes[:initial_size]]
+    steps = 0
+    rank_diffs = []
+    while (initial_size+steps) < len(train_set) - 1:
+        rank_diffs.append(rank_progressive(sub_train_set, validation_set))
+        policy_result = policy1(rank_diffs)
+        if policy_result != -1: break
+        steps += 1
+        sub_train_set.append(train_set[initial_size+steps])
+
+    return sub_train_set
+
+
+def wrapper_mre_progressive(train_set, validation_set, threshold=0.1):
+    initial_size = 10
+    training_indexes = range(len(train_set))
+    shuffle(training_indexes)
+    sub_train_set = [train_set[i] for i in training_indexes[:initial_size]]
+    steps = 0
+    while (initial_size+steps) < len(train_set) - 1:
+        mre_returned = mre_progressive(sub_train_set, validation_set)
+        if mre_returned < threshold: break
+        steps += 1
+        sub_train_set.append(train_set[initial_size+steps])
+
+    return sub_train_set
+
+
+def find_lowest_rank(train, test, bracket=10):
+    # Test data
+    train_independent = [t.decision for t in train]
+    train_dependent = [t.objective[-1] for t in train]
+
+    sorted_test = sorted(test, key=lambda x: x.objective[-1])
+    for r, st in enumerate(sorted_test): st.rank = r
+    test_independent = [t.decision for t in sorted_test]
+    test_dependent = [t.objective[-1] for t in sorted_test]
+
+    model = DecisionTreeRegressor()
+    model.fit(train_independent, train_dependent)
+    predicted = model.predict(test_independent)
+
+    predicted_id = [[i, p] for i, p in enumerate(predicted)]
+    predicted_sorted = sorted(predicted_id, key=lambda x: x[-1])
+    # assigning predicted ranks
+    predicted_rank_sorted = [[p[0], p[-1], i] for i,p in enumerate(predicted_sorted)]
+    select_few = predicted_rank_sorted[:10]
+    return [sf[0] for sf in select_few]
+
+
+
+if __name__ == "__main__":
+    datafolder = "./Data/"
+    evals_dict = {}
+    rank_diffs_dict = {}
+    stats_dict = {}
+    files = [datafolder + f for f in listdir(datafolder)]
+    for file in files:
+        print file
+        evals_dict[file] = []
+        rank_diffs_dict[file] = []
+        stats_dict[file] = {}
+        rank_rank_diffs = []
+        rank_evals = []
+        for _ in xrange(20):
+            print "+ ",
+            datasets = split_data(file)
+            train_set = datasets[0]
+            validation_set = datasets[1]
+            test_set = datasets[2]
+            sub_train_set_rank = wrapper_rank_progressive(train_set, validation_set)
+            lowest_rank = find_lowest_rank(sub_train_set_rank, test_set)
+            len_rank_train_set =  len(sub_train_set_rank)
+            min_rank_prog = min(lowest_rank)
+            rank_rank_diffs.append(min_rank_prog)
+            rank_evals.append(len_rank_train_set + len(validation_set) + 10)
+
+        evals_dict[file] = rank_evals
+        rank_diffs_dict[file] = rank_rank_diffs
+        stats_dict[file]["mean_rank_diff"] = np.mean(rank_rank_diffs)
+        stats_dict[file]["std_rank_diff"] = np.std(rank_rank_diffs)
+        stats_dict[file]["mean_evals"] = np.mean(rank_evals)
+        stats_dict[file]["std_evals"] = np.std(rank_evals)
+
+    import pickle
+    pickle.dump(evals_dict, "./PickleLocker/Progressive_Rank_Evals.p")
+    pickle.dump(rank_diffs_dict, "./PickleLocker/Progressive_Rank_Rank_Diff.p")
+    pickle.dump(stats_dict, "./PickleLocker/Progressive_Rank_Stats.p")
+
+
+
+
+
+
